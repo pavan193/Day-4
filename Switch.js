b840ded5776c2df5b13479ba2315c8707782c1c5
pavan193 <90746427+pavan193@users.noreply.github.com>
2021-09-20 17:51:23 +0530

    Add files via upload

---
+++ Switch.js
@@ -0,0 +1,27 @@
+var choice = 5;
+switch (choice) {
+    case 0:
+        console.log("Sunday");
+        break;
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Satday");
+        break;
+    default:
+        console.log("wrong input");
+        break;
+}
